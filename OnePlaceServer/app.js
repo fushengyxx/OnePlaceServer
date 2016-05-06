0ec4f16c6fd09a8e4436646fc91358dd8ebd491c
fushengyxx <fushengyxx@163.com>
2016-05-06 12:27:45 +0800

    连接mongoldb

--- OnePlaceServer/app.js
+++ OnePlaceServer/app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('.html',require('jade').__express);//两个下划线
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
---
+++ OnePlaceServer/database/db.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fushengyxx on 16/5/5.
+ */
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://phoesh.ddns.net/mongodb');//；连接数据库
+var Schema = mongoose.Schema;   //  创建模型
+var userScheMa = new Schema({
+    name: String,
+    password: String
+}); //  定义了一个新的模型，但是此模式还未和users集合有关联
+exports.user = db.model('users', userScheMa); //  与users集合关联
